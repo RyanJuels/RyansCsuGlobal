ef3f47b91422d315fe4adaedd056425194e5fd05
RyanJuels <79879891+RyanJuels@users.noreply.github.com>
2023-10-09 12:10:50 -0600

    updating final

--- ITS320 Week 8/ITS320_CTA8_Portfolio.py
+++ ITS320 Week 8/ITS320_CTA8_Portfolio.py
@@ -33,10 +33,34 @@ class automobile:
         while num < 1 and num > len(self.list_of_vehicles):
             num = input("Please enter a valid car you would like to update (enter number of car): ")
 
-        make = input("What is the new make of the car: ")
-        model = input("What is the new model of the car: ")
-        color = input("What is the new color of the car: ")
-        year = input("WHat is the new year of the car: ")
-        mileage = input("What is the new mileage of the car: ")
-
-        self.list_of_vehicles[num - 1] = vehicle(make, model, color, year, mileage)
+        print("1) Make")
+        print("2) Model")
+        print("3) Color")
+        print("4) Year")
+        print("5) Mileage")
+        print("6) Whole Car")
+        to_update = input("Which would you like to update: ")
+        while(to_update < 1 or to_update > 6):
+            input("Pleas choose a valid attribute to update: ")
+        if to_update == 1:
+            make = input("What is the new make of the car: ")
+            self.list_of_vehicles[num - 1].make = make
+        elif to_update == 2:
+            model = input("What is the new model of the car: ")
+            self.list_of_vehicles[num - 1].model = model
+        elif to_update == 3:
+            color = input("What is the new color of the car: ")
+            self.list_of_vehicles[num - 1].color = color
+        elif to_update == 4:
+            year = input("What is the new year of the car: ")
+            self.list_of_vehicles[num - 1].year = year
+        elif to_update == 5:
+            mileage = input("What is the new mileage of the car: ")
+            self.list_of_vehicles[num - 1].mileage = mileage
+        elif to_update == 6:
+            make = input("What is the new make of the car: ")
+            model = input("What is the new model of the car: ")
+            color = input("What is the new color of the car: ")
+            year = input("WHat is the new year of the car: ")
+            mileage = input("What is the new mileage of the car: ")
+            self.list_of_vehicles[num - 1] = vehicle(make, model, color, year, mileage)
