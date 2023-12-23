a4c0f22f9d4003ebcc5a3910e2c4a793ffdda56f
RyanJuels <79879891+RyanJuels@users.noreply.github.com>
2023-12-23 10:18:23 -0700

    adding Bank Gui

--- CSC372/CSC372_CTA2_Option1/src/BankBalanceGui.java
+++ CSC372/CSC372_CTA2_Option1/src/BankBalanceGui.java
@@ -1,32 +1,121 @@
-import javax.swing.*;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import java.awt.event.*;
+import javax.swing.JTextField;
+
+public class BankBalanceGui extends JFrame implements ActionListener {
+   private JLabel balanceLabel;
+   private JLabel amountLabel;
+   private JTextField totalBalance;
+   private JTextField amount;
+   private JButton setBalanceButton;
+   private JButton depositButton;
+   private JButton withdrawButton;
+
+   BankBalanceGui() {
+      GridBagConstraints positionConst = null;
+      setTitle("Salary");
+      balanceLabel = new JLabel("Balance:");
+      amountLabel = new JLabel("Amount:");
+
+      totalBalance = new JTextField(15);
+      totalBalance.setEditable(false);
+      totalBalance.setText("0");
+      
+      amount = new JTextField(15);
+      amount.setEditable(true);
+      amount.setText("0");
+
+      setBalanceButton = new JButton("Set Balance");
+      setBalanceButton.addActionListener(this);
+      
+      depositButton = new JButton("Deposit");
+      depositButton.addActionListener(this);
+      
+      withdrawButton = new JButton("Withdraw");
+      withdrawButton.addActionListener(this);
 
 
-public class BankBalanceGui {
-	
-	
-	private void GUIComponents()
-	{
-		JButton mybutton = new JButton("Please Click me!");
-		ActionListener mylistener = new ClickListener();
-		mybutton.addActionListener(mylistener);
-		JLabel mylabel = new JLabel("Hello!");
-		JPanel mypanel = new JPanel();
-		mypanel.add(mybutton);
-		mypanel.add(mylabel);
-		add(mypanel);
-	}
-	public static void main(String[] args)
-	{
-		JFrame myframe = new JFrame();
-		final int F_WIDTH = 250;
-		final int F_HEIGHT = 250;
-		myframe.setSize(F_WIDTH, F_HEIGHT);
-		myframe.setTitle("My Frame");
-		myframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		myframe.setVisible(true);
-	}
+      setLayout(new GridBagLayout());
+      positionConst = new GridBagConstraints();
+
+      positionConst.gridx = 0;
+      positionConst.gridy = 0;
+      positionConst.insets = new Insets(10, 10, 10, 10);
+      
+      add(balanceLabel, positionConst);
+
+      positionConst.gridx = 2;
+      positionConst.gridy = 0;
+      positionConst.insets = new Insets(10, 10, 10, 10);
+      add(totalBalance, positionConst);
+
+      positionConst.gridx = 0;
+      positionConst.gridy = 1;
+      positionConst.insets = new Insets(10, 10, 10, 10);
+      add(amountLabel, positionConst);
+
+      positionConst.gridx = 2;
+      positionConst.gridy = 1;
+      positionConst.insets = new Insets(10, 10, 10, 10);
+      add(amount, positionConst);
+      
+      positionConst.gridx = 0;
+      positionConst.gridy = 2;
+      positionConst.insets = new Insets(10, 10, 10, 10);
+      add(setBalanceButton, positionConst);
+      
+      positionConst.gridx = 1;
+      positionConst.gridy = 2;
+      positionConst.insets = new Insets(10, 10, 10, 10);
+      add(depositButton, positionConst);
+      
+      positionConst.gridx = 2;
+      positionConst.gridy = 2;
+      positionConst.insets = new Insets(10, 10, 10, 10);
+      add(withdrawButton, positionConst);
+   }
+
+   @Override
+   public void actionPerformed(ActionEvent event) {
+      String userInput;
+      String balanceAmount;
+      double outputAmount = 0;
+      
+      if(event.getSource() == setBalanceButton) {
+    	  userInput = amount.getText();
+    	  outputAmount = Double.parseDouble(userInput);
+    	  amount.setText("0");
+    	  
+      }
+      if(event.getSource() == depositButton) {
+    	  userInput = amount.getText();
+    	  balanceAmount = totalBalance.getText();
+    	  outputAmount = Double.parseDouble(balanceAmount) + Double.parseDouble(userInput);
+    	  amount.setText("0");
+      }
+      if(event.getSource() == withdrawButton) {
+    	  userInput = amount.getText();
+    	  balanceAmount = totalBalance.getText();
+    	  outputAmount = Double.parseDouble(balanceAmount) - Double.parseDouble(userInput);
+    	  amount.setText("0");
+      }
+      
+      totalBalance.setText(Double.toString(outputAmount));
+
+   }
+
+   public static void main(String[] args) {
+	   
+	  BankBalanceGui myFrame = new BankBalanceGui();
+
+      myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      myFrame.pack();
+      myFrame.setVisible(true);
+   }
 }
