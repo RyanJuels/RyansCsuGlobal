5369d975c8b25fe06ca9ece26b9a96587b090828
ryan.juelsgaard <ryan.juelsgaard@dish.com>
2024-03-18 12:04:00 -0600

    fixing based on comments

--- CSC400/CSC400CTA4/src/PostfixDriver.java
+++ CSC400/CSC400CTA4/src/PostfixDriver.java
@@ -5,7 +5,10 @@ public class PostfixDriver {
 		String secondString = "(a+b)/(c−d)";
 		String thirdString = "a/(b−c)*d";
 		String fourthString = "a−(b/(c−d)*e+f)^g";
-		String fifthString = "(a−b*c)/(d*e^f*g+h)";
+		String fifthString = "a−b*c/d*e^f*g+h";
+		String sixthString = "(a−b*c/d*e^f*g+h)";
+		String seventhString = "((a−b)*c/d*)e^f*g+h";
+		String eigthString = "a−b*c/d*e-f-g-h";
 		PostfixConverter t = new PostfixConverter();
 		
 		System.out.println("Infix : " + firstString + "\nPostfix: " + t.postfix(firstString) + "\n");
@@ -13,6 +16,9 @@ public class PostfixDriver {
 		System.out.println("Infix : " + thirdString + "\nPostfix: " + t.postfix(thirdString) + "\n");
 		System.out.println("Infix : " + fourthString + "\nPostfix: " + t.postfix(fourthString) + "\n");
 		System.out.println("Infix : " + fifthString + "\nPostfix: " + t.postfix(fifthString) + "\n");
+		System.out.println("Infix : " + sixthString + "\nPostfix: " + t.postfix(sixthString) + "\n");
+		System.out.println("Infix : " + seventhString + "\nPostfix: " + t.postfix(seventhString) + "\n");
+		System.out.println("Infix : " + eigthString + "\nPostfix: " + t.postfix(eigthString) + "\n");
 		
 	}
 }
