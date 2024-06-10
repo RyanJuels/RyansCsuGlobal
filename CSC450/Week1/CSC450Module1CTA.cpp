9498751544249ce0a93386d1cfffb03542b4a6a3
ryan.juelsgaard <ryan.juelsgaard@dish.com>
2024-06-10 12:22:30 -0600

    Adding CSC450 and Week 1

---
+++ CSC450/Week1/CSC450Module1CTA.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    string fName = "Mantis";
+    string lName = "Toboggan";
+    string address = "123 Main Street";
+    string city = "Philadelphia";
+    int zipCode = 19019;
+
+    cout << fName << " " << lName << endl;
+    cout << address << " " << city << ", " << zipCode << endl;
+
+    return 0;
+}
