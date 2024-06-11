da590abf814ddfc2f00d06a06c9f76bf692ae78a
ryan.juelsgaard <ryan.juelsgaard@dish.com>
2024-06-11 13:41:53 -0600

    adding comments

--- CSC450/Week1/CSC450Module1CTA.cpp
+++ CSC450/Week1/CSC450Module1CTA.cpp
@@ -3,12 +3,15 @@
 using namespace std;
 
 int main() {
+
+    // Variables to initialize
     string fName = "Mantis";
     string lName = "Toboggan";
     string address = "123 Main Street";
     string city = "Philadelphia";
     int zipCode = 19019;
 
+    // Print out my character
     cout << fName << " " << lName << endl;
     cout << address << " " << city << ", " << zipCode << endl;
 
