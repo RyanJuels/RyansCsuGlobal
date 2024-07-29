496ae6c235e90eac86d29eb453d57975f413d5af
ryan.juelsgaard <ryan.juelsgaard@dish.com>
2024-07-29 16:10:28 -0600

    fixing when threads join

--- CSC450/Week8/src/CounterThreads.java
+++ CSC450/Week8/src/CounterThreads.java
@@ -62,9 +62,8 @@ public class CounterThreads extends Thread {
 		
 		try {
 			t1.start();
-			t2.start();
-			
 			t1.join();
+			t2.start();
 			t2.join();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
